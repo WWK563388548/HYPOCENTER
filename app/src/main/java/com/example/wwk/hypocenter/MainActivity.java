65813236b2d4d9d431e2ede90e7800b534373c45
wc563388548@gmail.com <wc563388548@gmail.com>
2017-05-08 09:04:51 +0900

    Create QueryUtils to extract information from Json, and revise MainActivity

--- app/src/main/java/com/example/wwk/hypocenter/MainActivity.java
+++ app/src/main/java/com/example/wwk/hypocenter/MainActivity.java
@@ -16,16 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Create a fake list of earthquake locations.
-        ArrayList<Earthquake> earthquakes = new ArrayList<>();
-
-        earthquakes.add(new Earthquake("7.2", "Peking", "Feb 07, 2017"));
-        earthquakes.add(new Earthquake("7.2", "Tokyo", "Feb 07, 2017"));
-        earthquakes.add(new Earthquake("7.2", "Paris", "Feb 07, 2017"));
-        earthquakes.add(new Earthquake("7.2", "London", "Feb 07, 2017"));
-        earthquakes.add(new Earthquake("7.2", "San Francisco", "Feb 07, 2017"));
-        earthquakes.add(new Earthquake("7.2", "Mexico City", "Feb 07, 2017"));
-        earthquakes.add(new Earthquake("7.2", "Rio de Janeiro", "Feb 07, 2017"));
-        earthquakes.add(new Earthquake("7.2", "Moscow", "Feb 07, 2017"));
+        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
