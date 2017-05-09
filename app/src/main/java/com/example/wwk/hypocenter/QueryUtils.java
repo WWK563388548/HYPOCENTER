0d85197b79cde9c2c53dc3e9c7278192b2f742a2
wc563388548@gmail.com <wc563388548@gmail.com>
2017-05-09 21:49:36 +0900

    Using Loader to solve shortcoming of AsyncTask

--- app/src/main/java/com/example/wwk/hypocenter/QueryUtils.java
+++ app/src/main/java/com/example/wwk/hypocenter/QueryUtils.java
@@ -24,6 +24,7 @@ import java.util.List;
 
 public final class QueryUtils {
 
+    // Tag for log message
     private static final String LOG_TAG = QueryUtils.class.getSimpleName();
 
     private QueryUtils() {
