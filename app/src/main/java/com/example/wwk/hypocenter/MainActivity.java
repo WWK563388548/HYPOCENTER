0d5b9d20458bb01c3680f78a4727cbd69b95443e
wc563388548@gmail.com <wc563388548@gmail.com>
2017-05-10 17:49:54 +0900

    Create EarthquakePreferenceFragment by PreferenceFragment

--- app/src/main/java/com/example/wwk/hypocenter/MainActivity.java
+++ app/src/main/java/com/example/wwk/hypocenter/MainActivity.java
@@ -10,6 +10,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -91,7 +92,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             // Update empty state with no connection error message
             mEmptyStateTextView.setText(R.string.no_internet_connection);
         }
-
     }
 
     @Override
@@ -127,6 +127,17 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
---
+++ app/src/main/java/com/example/wwk/hypocenter/SettingActivity.java
@@ -0,0 +1,22 @@
+package com.example.wwk.hypocenter;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by wwk on 17/5/10.
+ */
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    public static class EarthquakePreferenceFragment extends PreferenceFragment {
+
+    }
+}
