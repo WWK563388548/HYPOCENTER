1e56ea4de9d4c2df555c184a8789dc6f3fc6c759
wc563388548@gmail.com <wc563388548@gmail.com>
2017-05-10 17:20:24 +0900

    add new menu

--- app/src/main/java/com/example/wwk/hypocenter/MainActivity.java
+++ app/src/main/java/com/example/wwk/hypocenter/MainActivity.java
@@ -9,6 +9,7 @@ import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -122,6 +123,11 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
 }
 
